1f513847c182a7c9f07a402f0fa58ac281b49bf5
nacho <nacho.nunez@aoifes.com>
2018-09-04 07:34:37 -0600

    add user-overrides.js

---
+++ user-overrides.js
@@ -0,0 +1,46 @@
+user_pref("browser.startup.page", 3);
+user_pref("network.http.spdy.enabled", true);
+user_pref("network.http.spdy.enabled.deps", true);
+user_pref("network.http.spdy.enabled.http2", true);
+user_pref("keyword.enabled", true);
+user_pref("browser.urlbar.speculativeConnect.enabled", true);
+user_pref("browser.urlbar.autocomplete.enabled", true);
+user_pref("browser.urlbar.suggest.history", true);
+user_pref("browser.urlbar.suggest.openpage", true);
+user_pref("browser.urlbar.autoFill", true);
+user_pref("browser.urlbar.oneOffSearches", true);
+user_pref("browser.formfill.enable", true);
+user_pref("places.history.enabled", true);
+user_pref("browser.cache.disk.enable", true);
+user_pref("browser.cache.disk.capacity", 358400);
+user_pref("browser.cache.disk.smart_size.enabled", true);
+user_pref("browser.cache.disk.smart_size.first_run", true);
+user_pref("browser.cache.disk_cache_ssl", true);
+user_pref("browser.sessionhistory.max_total_viewers", -1);
+user_pref("browser.sessionstore.max_tabs_undo", 10);
+user_pref("browser.sessionstore.max_windows_undo", 3);
+user_pref("browser.sessionstore.privacy_level", 0);
+user_pref("browser.sessionstore.resume_from_crash", true);
+user_pref("browser.shell.shortcutFavicons", true);
+user_pref("security.mixed_content.block_display_content", false);
+user_pref("layers.acceleration.disabled", false);
+user_pref("dom.webaudio.enabled", true);
+user_pref("browser.download.useDownloadDir", true);
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", false); // see note above
+user_pref("privacy.clearOnShutdown.formdata", false); // Form & Search History
+user_pref("privacy.clearOnShutdown.history", false); // Browsing & Download History
+user_pref("privacy.clearOnShutdown.offlineApps", false); // Offline Website Data
+user_pref("privacy.clearOnShutdown.sessions", false); // Active Logins
+user_pref("browser.startup.homepage_override.mstone", "ignore"); // master switch
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("startup.homepage_override_url", ""); // What's New page after updates
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
