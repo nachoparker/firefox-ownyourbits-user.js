865b0291dfeff22c53362ea9710648724fa75099
nacho <nacho@ownyourbits.com>
2018-09-09 21:32:11 -0600

    [ownyourbits] close window with last tab

--- user.js
+++ user.js
@@ -1689,7 +1689,7 @@ user_pref("_user.js.parrot", "5000 syntax error: this is an ex-parrot!");
 /* UX BEHAVIOR ***/
    // user_pref("browser.backspace_action", 2); // 0=previous page, 1=scroll up, 2=do nothing
    // user_pref("browser.ctrlTab.previews", true);
-   user_pref("browser.tabs.closeWindowWithLastTab", false);
+   // user_pref("browser.tabs.closeWindowWithLastTab", false);
    // user_pref("browser.tabs.loadBookmarksInTabs", true); // open bookmarks in a new tab (FF57+)
    // user_pref("browser.urlbar.decodeURLsOnCopy", true); // see  Bugzilla 1320061 (FF53+)
    // user_pref("general.autoScroll", false); // middle-click enabling auto-scrolling [WINDOWS] [MAC]
